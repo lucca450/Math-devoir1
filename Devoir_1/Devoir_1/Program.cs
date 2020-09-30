f5ee0c09e30fc1f0df8426f9e13c4393cd8cabb3
lucca possa silva <luccapossa450@gmail.com>
2020-09-30 17:34:22 -0400

    ajouter certaines vérifications de régles:
    NOTE:Le formatdoit emprunterune symbolique approximative de ceque nous
    avons vu en classe :
    Utilisez « 0 » et « 1 » comme terminaux, uniquement;
    Utilisez les lettres pour les non terminaux (A, B, C, …);
    Utilisez « -> », en guise de flèche représentant la
    dérivation;
    Utilisez «e» pourla chaîne vide;
    Évidemment vous devez vous assurer de n’accepter que les règles qui sont bien
    formées. N’oubliez pas qu’il doit y avoir au
    minimum une règle qui part du non-terminal S.

--- Devoir_1/Devoir_1/Program.cs
+++ Devoir_1/Devoir_1/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Devoir_1
 {
@@ -30,32 +30,64 @@ namespace Devoir_1
             bool done = false;
             while (!done)                                                           //  Tant que l'utilisateur n'a pas fini d'écrire de règle
             {
-                Console.WriteLine("Entrer une règle à la grammaire");
+                Console.WriteLine("Entrez une règle à la grammaire");
                 string rule = Console.ReadLine().Trim();                                //  Lecture de la règle
 
-                sw.WriteLine(rule);                                                     //  Écriture de la règle dans le fichier
 
-                Console.WriteLine(string.Format("Règle ajoutée: {0}", rule));
-
-                validInput = false;
-                while (!validInput)                                                     //  Tant que l'utiliseur n'a pas de entrée de donnée valide
+                if (Regex.IsMatch(rule, "^(?:[A-Z]->[0-1]{1}[A-Z]{1}|[A-Z]->[0-1]{1}|S->e)")) 
                 {
-                    validInput = true;
+                           
+                    sw.WriteLine(rule);                                                     //  Écriture de la règle dans le fichier
 
-                    Console.WriteLine("Voulez-vous ajouter une autre règle? o/n");
-                    string input = Console.ReadLine();                                      //  Entrée de l'utilisateur
-                    switch (input)
+                    Console.WriteLine(string.Format("Règle ajoutée: {0}", rule));
+
+                    validInput = false;
+                    int foundSRule = 0;
+                    while (!validInput)                                                     //  Tant que l'utiliseur n'a pas de entrée de donnée valide
                     {
-                        case "o":                                                           //  Si oui
-                            break;
-                        case "n":                                                           //  Si non
-                            done = true;
-                            break;
-                        default:                                                            //  Si entrée invalide
-                            Console.WriteLine("Entrée invalide");
-                            validInput = false;
-                            break;
+                        validInput = true;
+
+                        Console.WriteLine("Voulez-vous ajouter une autre règle? o/n");
+                        string input = Console.ReadLine();                                      //  Entrée de l'utilisateur
+                        
+                        switch (input)
+                        {
+                            case "o":                                                           //  Si oui
+                                break;
+                            case "n":                                                           //  Si non
+                                sw.Close();
+                                string readText = File.ReadAllText(path);
+      
+                                String[] rulesList = readText.Split("\r\n");                //Divise ligne par ligne
+                                    
+
+                                    foreach (String r in rulesList)                             //Cherche une règle S
+                                    {
+                                        if (r != "")
+                                        {
+                                            if (r.Substring(0, 1) == "S")
+                                            {
+                                                foundSRule++;
+                                            }
+                                        }
+                                    }
+                                
+
+                                if (foundSRule > 0)                                             //Termine si au moins une des règles est une règle S
+                                {
+                                    done = true;
+                                }                 
+                                break;
+                            default:                                                            //  Si entrée invalide
+                                Console.WriteLine("Entrée invalide");
+                                validInput = false;
+                                break;
+                        }
                     }
+                }
+                else
+                {
+                    Console.WriteLine("La règle n'est pas valide. Respectez le format suivant: S->e OU X->1 OU X->1X");
                 }
             }
 
@@ -87,6 +119,9 @@ namespace Devoir_1
         static void ImportGrammar()
         {
 
+            string readText = File.ReadAllText(@".\Grammaire.txt");
+            Console.WriteLine("Voici ce que contient la grammaire chargée: \n" + readText);
+
         }
 
         static void Main(string[] args)
@@ -95,10 +130,11 @@ namespace Devoir_1
 
             while (invalidInput)                                                        //  Tant que l'utiliseur n'a pas de entrée de donnée valide
             {
-                Console.WriteLine("Choisissez parmis les fonctions suivantes :");       //  Menu de l'application
+                Console.WriteLine("Choisissez parmis les fonctions suivantes :");       //  Menu principal de l'application
                 Console.WriteLine("1- Créer une nouvelle grammaire");
                 Console.WriteLine("2- Charger une grammaire existante");
-                Console.WriteLine("3- Quitter");
+                Console.WriteLine("3- Visualiser la grammaire actuelle");
+                Console.WriteLine("4- Quitter");
 
                 string input = Console.ReadLine();                                      //  Lecture de la réponse
 
@@ -110,7 +146,10 @@ namespace Devoir_1
                     case "2":                                                               // Si 2 : Importation de la grammaire
                         ImportGrammar();
                         break;
-                    case "3":                                                               // Si 3 : Fermeture de l'application
+                    case "3":                                                               // Si 3 : Visualisation de la grammaire
+                        
+                        break;
+                    case "4":                                                               // Si 3 : Fermeture de l'application
                         invalidInput = false;
                         break;
                     default:                                                                // Si entrée invalide
