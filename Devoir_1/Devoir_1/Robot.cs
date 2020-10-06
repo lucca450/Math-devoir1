36547276a2b663113374fdace6cc037f73f56691
lucca possa silva <luccapossa450@gmail.com>
2020-10-05 21:49:47 -0400

    reste encore surement des cas que j'Ai pas testé, mais ca avance..

--- Devoir_1/Devoir_1/Robot.cs
+++ Devoir_1/Devoir_1/Robot.cs
@@ -27,6 +27,9 @@ namespace Devoir_1
             char firstLetter, nextLetter, terminal;
             List<Node> myNodes = new List<Node>();
 
+            bool needToCreateFnode = false;
+            bool foundSameLetter = false;
+            Node nodeWithSameLetter = null;
             foreach (string rule in grammar.rules)
             {
                 bool sRule, noLetterRule, normalRule, emptySRule, noLetterSRule;
@@ -48,11 +51,13 @@ namespace Devoir_1
                                 if (rule[0] == node.letter)
                                 {
                                     node.nodeLinks.Add(new Link(rule[0], rule[3], 'F'));
+                                    needToCreateFnode = true;
                                 }
                                 else
                                 {
                                     Node n0 = new Node(rule[0], false, new Link(rule[0], rule[3], 'F'));
                                     myNodes.Add(n0);
+                                    needToCreateFnode = true;
                                 }
                                 break;
                             }
@@ -61,8 +66,10 @@ namespace Devoir_1
                         {
                             Node n0 = new Node(rule[0], false, new Link(rule[0], rule[3], 'F'));
                             myNodes.Add(n0);
+                            needToCreateFnode = true;
                         }
                         break;
+
                     case true when sRule:
                         int h = 0;
 
@@ -87,30 +94,126 @@ namespace Devoir_1
                             Node n1 = new Node(rule[0], false, new Link(rule[0], rule[3], char.Parse(rule.Substring(rule.Length - 1))));
                             myNodes.Add(n1);
                         }
-
-
-
                         break;
                     case true when noLetterRule && !noLetterSRule:
                         int j = 0;
-                        Node n2 = new Node(rule[0],false, new Link(rule[0], rule[3], 'F'));
-                        myNodes.Add(n2);
+                        foundSameLetter = false;
+                        nodeWithSameLetter = null;
+                        if (myNodes.Count != 0)
+                        {
+                            foreach (var node in myNodes)
+                            {
+                                if (rule[0] == node.letter)
+                                {
+                                    foundSameLetter = true;
+                                    nodeWithSameLetter = node;
+                                    needToCreateFnode = true;
+                                    /*node.nodeLinks.Add(new Link(rule[0], rule[3], 'F'));
+                                    needToCreateFnode = true;*/
+                                }
+                                else
+                                {
+                                    /* Node n2 = new Node(rule[0], false, new Link(rule[0], rule[3], 'F'));
+                                     myNodes.Add(n2);
+                                     needToCreateFnode = true;*/
+                                    needToCreateFnode = true;
+                                }
+                                //break;
+                            }
+
+                            if (foundSameLetter == true)
+                            {
+                                nodeWithSameLetter.nodeLinks.Add(new Link(rule[0], rule[3], 'F'));
+                            }
+                            else
+                            {
+                                Node n2 = new Node(rule[0], false, new Link(rule[0], rule[3], 'F'));
+                                myNodes.Add(n2);
+                            }
+                        }
+                        else
+                        {
+                            Node n2 = new Node(rule[0], false, new Link(rule[0], rule[3], 'F'));
+                            myNodes.Add(n2);
+                            needToCreateFnode = true;
+                        }
                         break;
                     case true when normalRule && !sRule:
                         int k = 0;
-                        Node n3 = new Node(rule[0], false, new Link(rule[0], rule[3], char.Parse(rule.Substring(rule.Length - 1))));
-                        myNodes.Add(n3);
+                        foundSameLetter = false;
+                        nodeWithSameLetter = null;
+                        if (myNodes.Count != 0)
+                        {
+                            foreach (var node in myNodes)
+                            {
+                                if (rule[0] == node.letter)
+                                {
+                                    foundSameLetter = true;
+                                    nodeWithSameLetter = node;
+                                    //node.nodeLinks.Add(new Link(rule[0], rule[3], char.Parse(rule.Substring(rule.Length - 1))));
+                                }
+                                else
+                                {
+                                    /*Node n3 = new Node(rule[0], false, new Link(rule[0], rule[3], char.Parse(rule.Substring(rule.Length - 1))));
+                                    myNodes.Add(n3);*/
+
+                                }
+                                //break;
+                            }
+
+                            if (foundSameLetter == true)
+                            {
+                                nodeWithSameLetter.nodeLinks.Add(new Link(rule[0], rule[3], char.Parse(rule.Substring(rule.Length - 1))));
+                            }
+                            else
+                            {
+                                Node n3 = new Node(rule[0], false, new Link(rule[0], rule[3], char.Parse(rule.Substring(rule.Length - 1))));
+                                myNodes.Add(n3);
+                            }
+                        }
+                        else
+                        {
+                            Node n3 = new Node(rule[0], false, new Link(rule[0], rule[3], char.Parse(rule.Substring(rule.Length - 1))));
+                            myNodes.Add(n3);
+                        }
                         break;
                     case true when emptySRule:
                         int l = 0;
-                        //je pense que lorsqu'on a un S->e, sans aucun autre S->XY, ca veut dire que la grammaire est tjrs e(vide) donc S sera relié à aucun noeud.
+                        if (myNodes.Count != 0)
+                        {
+                            foreach (var node in myNodes)
+                            {
+                                if (rule[0] == node.letter)
+                                {
+                                    node.nodeLinks.Add(new Link(rule[0], rule[3], 'S'));
+                                    node.isFinal = true;
+                                }
+                                else
+                                {
+                                    Node n4 = new Node(rule[0], true, new Link(rule[0], rule[3], 'S'));
+                                    myNodes.Add(n4);
+                                }
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            Node n4 = new Node(rule[0], true, new Link(rule[0], rule[3], 'S'));
+                            myNodes.Add(n4);
+                        }
                         break;
                 }
+
+
+            }
+            if (needToCreateFnode == true)
+            {
+                Node n5 = new Node('F', true);
+                myNodes.Add(n5);
             }
 
 
-
-           //Console.WriteLine( String.Format("|{0,10}|{1,10}|{2,10}|{3,10}|", myNodes[0].letter, myNodes[1].letter, myNodes[2].letter, myNodes[3].letter));
+            //Console.WriteLine( String.Format("|{0,10}|{1,10}|{2,10}|{3,10}|", myNodes[0].letter, myNodes[1].letter, myNodes[2].letter, myNodes[3].letter));
 
 
             foreach (var node in myNodes)
@@ -121,11 +224,14 @@ namespace Devoir_1
                 Console.WriteLine(node.letter + ": ");
                 foreach (var link in node.nodeLinks)
                 {
-                    Console.WriteLine("From " + link.from + " using " + link.road + " to " + link.to + ": ");
+                    Console.WriteLine("From " + link.from + " using " + link.road + " to " + link.to);
                 }
             }
 
-
+            foreach (var node in myNodes)
+            {
+                Console.WriteLine(node.letter + ": final? " + node.isFinal);
+            }
 
             /*
             foreach (var link in node.nodeLinks)
@@ -135,7 +241,7 @@ namespace Devoir_1
             */
 
 
-           
+
             return null;
         }
     }
